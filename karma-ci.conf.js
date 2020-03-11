b3af3dd5fa1283e066f756d709be968465bafde1
Felix Waage <felix.waage@sap.com>
2020-03-11 10:33:28 +0100

    pipeline update

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -35,7 +35,7 @@ module.exports = function (config) {
 
 		browserNoActivityTimeout: 30000,
 
-		browsers: ['ChromeHeadless', 'RemoteChrome'],
+		browsers: ['RemoteChrome'],
 
 		hostname: "karma",
 		customLaunchers: {
