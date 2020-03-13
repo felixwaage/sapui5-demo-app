5a2cd4fa3b630f6d97e36da25487a771b63be7c8
Felix Waage <felix.waage@sap.com>
2020-03-13 15:45:19 +0100

    just backup

--- Integration/index.js
+++ Integration/index.js
@@ -1,8 +1,51 @@
 var fs = require('fs');
+var request = require('request');
 
 console.log("Start of SAPUI5 integration test modul... ok");
 
-var xmlPath = "./webapp/view/Home.view.xml";
+var xmlPath = "./Integration/manifest.json";
 var xmlContent = fs.readFileSync(xmlPath,"utf8");
 
-console.log(xmlContent);
+//=========================================================================
+
+var dataSources = getDataSourcesFromManifest(xmlContent);
+checkODataService(dataSources);
+
+function callODataService(url) {
+    request( url , { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        if (body.status === 200) { return body; }
+    });
+}
+
+function checkODataService(dataSources) {
+    var dataSourcesElements = Object.values(dataSources);
+    var dataSourcesElementsNames = Object.keys(dataSources);
+
+    var results = [];
+
+    for(var i = 0; i < dataSourcesElements.length; i++) {
+        var element = dataSourcesElements[i];
+        var elementName = dataSourcesElementsNames[i];
+
+        var status = false;
+        if(body.status === 200) { status = true; }
+        else { status = false; } 
+
+        result = {
+            name: elementName,
+            statusCode: body.status,
+            status: 
+        }
+
+        console.log('BREAK POINT');
+    }
+
+    console.log(dataSourcesElements);
+}
+
+function getDataSourcesFromManifest(file) {
+    var manifestObj = JSON.parse(xmlContent);
+    var dataSources = manifestObj["sap.app"].dataSources;
+    return dataSources;
+}
